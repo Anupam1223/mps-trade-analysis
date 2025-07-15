9ac9fcd68a6e1685d4f91cb84c561e8329f62871
Anupam Siwakoti <asiwakoti@Mac-Pro.local>
2025-07-15 15:18:29 -0500

    model is kind of stable but a lot of work has to be done

--- src/model.py
+++ src/model.py
@@ -2,64 +2,86 @@ import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
 import tensornetwork as tn
+from tensorflow.keras import regularizers
+from tensorflow.keras.layers import Dropout
+
+# --- THIS IS THE FIX ---
+# Explicitly set the backend to TensorFlow before creating any TN objects.
+tn.set_default_backend("tensorflow")
+
+def directional_accuracy(y_true, y_pred):
+    """Calculates directional accuracy for a classification problem."""
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(y_pred, tf.float32)
+    
+    correct_direction = tf.equal(tf.sign(y_true - 0.5), tf.sign(y_pred - 0.5))
+    return tf.reduce_mean(tf.cast(correct_direction, tf.float32))
+
+
 
 # A custom Keras Layer for a simple MPS contraction
 class MPSLayer(tf.keras.layers.Layer):
-    def __init__(self, bond_dim, output_dim=1, **kwargs):
+    def __init__(self, bond_dim, output_dim=1, l2_lambda=1e-4, **kwargs):
         super(MPSLayer, self).__init__(**kwargs)
         self.bond_dim = bond_dim
         self.output_dim = output_dim
+        self.l2_lambda = l2_lambda
 
     def build(self, input_shape):
-        # input_shape is (batch_size, num_sites, feature_dim)
         num_sites = input_shape[1]
         feature_dim = input_shape[2]
-        
-        # Create the MPS tensors as trainable weights
+        reg = regularizers.l2(self.l2_lambda)
+
         self.mps_tensors = []
-        # First tensor
-        self.mps_tensors.append(self.add_weight(shape=(feature_dim, self.bond_dim), initializer='glorot_uniform', name='mps_0'))
-        # Middle tensors
+        self.mps_tensors.append(
+            self.add_weight(shape=(feature_dim, self.bond_dim),
+                            initializer='glorot_uniform',
+                            regularizer=reg,
+                            name='mps_0'))
         for i in range(1, num_sites - 1):
-            self.mps_tensors.append(self.add_weight(shape=(self.bond_dim, feature_dim, self.bond_dim), initializer='glorot_uniform', name=f'mps_{i}'))
-        # Last tensor
-        self.mps_tensors.append(self.add_weight(shape=(self.bond_dim, feature_dim), initializer='glorot_uniform', name=f'mps_{num_sites-1}'))
+            self.mps_tensors.append(
+                self.add_weight(shape=(self.bond_dim, feature_dim, self.bond_dim),
+                                initializer='glorot_uniform',
+                                regularizer=reg,
+                                name=f'mps_{i}'))
+        self.mps_tensors.append(
+            self.add_weight(shape=(self.bond_dim, feature_dim),
+                            initializer='glorot_uniform',
+                            regularizer=reg,
+                            name=f'mps_{num_sites-1}'))
 
     def call(self, inputs):
-        # inputs is the batch of time series data
-        batch_size = tf.shape(inputs)[0]
-        
-        # This function processes one sample at a time
         def contract_one_sample(sample):
-            # sample shape: (num_sites, feature_dim)
-            nodes = [tn.Node(t) for t in self.mps_tensors]
-            
-            # Contract input features into the MPS
-            # Start with the first site
-            result = tf.tensordot(sample[0], nodes[0].tensor, axes=[[0], [0]])
-            
-            # Contract the rest of the sites
-            for i in range(1, len(nodes)):
-                result = tf.tensordot(result, nodes[i].tensor, axes=[[-1], [0]])
-                result = tf.tensordot(result, sample[i], axes=[[-1], [0]])
-
+            result = tf.tensordot(sample[0], self.mps_tensors[0], axes=[[0], [0]])
+            for i in range(1, len(self.mps_tensors)):
+                mps_tensor = self.mps_tensors[i]
+                feature_vec = sample[i]
+                if len(mps_tensor.shape) == 3:
+                    axes = [[1], [0]]
+                else:
+                    axes = [[1], [0]]
+                op_i = tf.tensordot(mps_tensor, feature_vec, axes=axes)
+                result = tf.tensordot(result, op_i, axes=[[-1], [0]])
             return result
 
-        # Use tf.map_fn to apply the contraction to each sample in the batch
-        batch_results = tf.map_fn(contract_one_sample, inputs, dtype=tf.float32)
-        return batch_results
+        batch_results = tf.map_fn(
+            contract_one_sample, inputs, fn_output_signature=tf.float32
+        )
+        return tf.expand_dims(batch_results, axis=-1)
 
-def build_model(input_shape, bond_dim=8):
-    """Builds and compiles the Keras model with the MPS layer."""
+
+def build_model(input_shape, bond_dim=8, dropout_rate=0.3):
     inputs = Input(shape=input_shape)
-    # The MPS layer contracts the time series down to a single feature vector
     mps_output = MPSLayer(bond_dim=bond_dim)(inputs)
-    # A final dense layer for regression
-    outputs = Dense(1)(mps_output)
+    mps_output = Dropout(dropout_rate)(mps_output)
     
+    # Binary classification output with sigmoid
+    outputs = Dense(1, activation='sigmoid')(mps_output)
+
     model = Model(inputs, outputs)
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    
-    print(f"Model built with bond dimension {bond_dim}.")
-    model.summary()
+    model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['accuracy', directional_accuracy]  # you can keep directional_accuracy as extra
+    )
     return model
